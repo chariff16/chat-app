be01298c5a0e53c33ec1cbf1705164a737937b7a
chariff16 <lotfibusi@gmail.com>
2025-03-01 21:22:54 +0100

    changing the structure of the message body

--- src/components/Messages/MessagesBody.vue
+++ src/components/Messages/MessagesBody.vue
@@ -2,22 +2,35 @@
     <div class="flex flex-col bg-[#F5F5F5]">
         <MessageHeader />
         <div class="h-div">
-            <MessagesList />
+            <MessagesList :messagesList="messagesList" :senderName="sendName" :userInfo="userInfo" />
         </div>
         <InputFiled />
     </div>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 import MessageHeader from '@/components/Messages/MessageHeader.vue';
 import InputFiled from '@/components/Messages/InputFiled.vue';
-import { useRoute, useRouter } from 'vue-router';
 import MessagesList from '@/components/Messages/MessagesList.vue';
+import messages from '@/assets/data/messages.json';
+import users from '@/assets/data/users.json'
 
 
 
 const route = useRoute();
 const router = useRouter();
+
+
+const userInfo = users.find(user => user.id == route.params.id);
+const messagesList = computed(() => {
+    return messages[route.params.id].message;
+});
+
+const sendName = computed(() => {
+    return messages[route.params.id].name;
+})
 
 
 </script>
--- src/components/Messages/MessagesList.vue
+++ src/components/Messages/MessagesList.vue
@@ -4,10 +4,10 @@
             <div class="flex justify-between items-center px-[5%]">
                 <div class="flex gap-2 ">
                     <div class=" h-[3rem] w-[3rem] rounded-full relative bg-cover bg-center bg-no-repeat"
-                        :style="`background-image : url('${User.profilePic}')`"></div>
+                        :style="`background-image : url('${props.userInfo.profilePic}')`"></div>
                     <div class="text-white">
                         <p class="font-medium">
-                            {{ User.name }}
+                            {{ props.userInfo.name }}
                         </p>
                         <p class="text-[#A1A1A1] text-[0.75rem]">
                             {{ selectedImage.datestamp }} at {{ selectedImage.timestamp }}
@@ -23,10 +23,10 @@
             </div>
         </div>
     </Teleport>
-    <div v-for="msg in messages[route.params.id].message" class="flex my-2"
-        :class="msg.sender === messages[route.params.id].name ? 'justify-start' : ' justify-end'">
+    <div v-for="msg in props.messagesList" class="flex my-2"
+        :class="msg.sender === props.senderName ? 'justify-start' : ' justify-end'">
         <div v-if="msg.type === 'text'" class="p-1 w-fit rounded-xl"
-            :class="msg.sender === messages[route.params.id].name ? 'bg-[#E8E8E8]' : ' bg-[#6852D6] text-white'">
+            :class="msg.sender === props.senderName ? 'bg-[#E8E8E8]' : ' bg-[#6852D6] text-white'">
             <div class="p-2">
                 <p class="">
                     {{ msg.content }}
@@ -37,7 +37,7 @@
             </div>
         </div>
         <div v-if="msg.type === 'image'" class="p-1 w-fit rounded-xl"
-            :class="msg.sender === messages[route.params.id].name ? 'bg-[#E8E8E8]' : ' bg-[#6852D6] text-white'">
+            :class="msg.sender === props.senderName ? 'bg-[#E8E8E8]' : ' bg-[#6852D6] text-white'">
             <img :src="msg.content" alt="" class="rounded-xl cursor-pointer" @click="setSelectedImage(msg)">
             <p class="text-sm  text-[0.625rem] text-end mt-1">
                 {{ msg.timestamp }}
@@ -47,14 +47,20 @@
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
-import messages from '@/assets/data/messages.json';
-import users from '@/assets/data/users.json'
 import { ref } from 'vue';
 
+const props = defineProps({
+    messagesList: {
+        type: Object
+    },
+    senderName: {
+        type: String
+    },
+    userInfo: {
+        type: Object
+    }
+})
 
-const route = useRoute();
-const User = users.find(user => user.id == route.params.id);
 
 const selectedImage = ref(null);
 const imageModalStatus = ref(false);
